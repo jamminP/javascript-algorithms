c4a20cebe926bb7bae7b744ddb9db5271db0ff75
JAEMIN PARK <117453101+jamminP@users.noreply.github.com>
2025-05-14 17:30:46 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 44.48 ms, Memory: 65.6 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -10,17 +10,39 @@ function solution(participant, completion) {
     if(!participant.every((name => /^[a-z]{1,20}$/.test(name))))
         return false;
     
-    participant.sort();
-    completion.sort();
+    // 1. sort()를 사용한 방법
+//     participant.sort();
+//     completion.sort();
     
-    console.log(participant);
-    console.log(completion);
+//     console.log(participant);
+//     console.log(completion);
     
-    for(let i = 0; i < participant.length; i++){
-        if(participant[i] !== completion[i]){
-            answer = participant[i];
-            break;
+//     for(let i = 0; i < participant.length; i++){
+//         if(participant[i] !== completion[i]){
+//             answer = participant[i];
+//             break;
+//         }
+//     }
+    
+    const map = new Map(); // map을 생성.
+    
+    for (const namePar of completion){
+        map.set(namePar, (map.get(namePar) || 0) + 1); 
+        // "namePar" : (이렇게 한 이유는 get이 없으면 undefined를 return 하기 때문) 
+        // || 연산자는 a || b 일경우, a가 T이면 a값 반환 / a가 F이면 b값 반환.
+    }
+    
+    // completion 기준 map생성 완료. { 'eden' => 1, 'kiki' => 1 }
+    
+    for(const nameCom of participant){
+        // 조건1. key값이 없거나,
+        // 조건2. value 값이 0인 경우, 
+        if(!map.has(nameCom) || map.get(nameCom) === 0){
+            answer += nameCom;
+            break; // 1명 이기 때문에 loop문을 바로 종료 시킴. 
         }
+        
+        map.set(nameCom, map.get(nameCom) - 1); // 값이 새로 생기는것이 아닌 새 값으로 교체가 이루어짐.
     }
     
     return answer;
