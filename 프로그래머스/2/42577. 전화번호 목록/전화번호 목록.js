82df140c1a8db6e67f1f5b456877cb7df6058b86
JAEMIN PARK <117453101+jamminP@users.noreply.github.com>
2025-05-16 17:20:34 +0900

    [level 2] Title: 전화번호 목록, Time: 167.46 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,20 +1,29 @@
 function solution(phone_book) {
     var answer = true;
     
-    const phoneBook = new Set(phone_book); // 같은 전화번호가 중복해서 들어있지 않게 하기 위해서.
+//     const phoneBook = new Set(phone_book); // 같은 전화번호가 중복해서 들어있지 않게 하기 위해서.
     
-    for(mainNum of phoneBook){
-        let dataString = "";  
+//     for(mainNum of phoneBook){
+//         let dataString = "";  
         
-        for(num of mainNum){
-            dataString += num;
+//         for(num of mainNum){
+//             dataString += num;
             
-            if(mainNum !== dataString && phoneBook.has(dataString)){ // 자기 자신을 제외하고 && 접두사가 존재하는 경우. 자기자신의 일부가 어딘가 있는지 찾기.
-                return false;
-            }
+//             if(mainNum !== dataString && phoneBook.has(dataString)){ // 자기 자신을 제외하고 && 접두사가 존재하는 경우. 자기자신의 일부가 어딘가 있는지 찾기.
+//                 return false;
+//             }
+//         }
+//     }
+    
+    
+    phone_book.sort();
+    
+    for(let i = 0; i < phone_book.length - 1; i++){
+        if(phone_book[i + 1].startsWith(phone_book[i])){
+            answer = false;
+            break;
         }
     }
-    
     
     return answer;
 }
