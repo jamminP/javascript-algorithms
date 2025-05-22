f0eababe132c4fb06c14a260747c76be3f5ca5ad
JAEMIN PARK <117453101+jamminP@users.noreply.github.com>
2025-05-22 16:58:14 +0900

    [level 2] Title: 숫자의 표현, Time: 2.63 ms, Memory: 36.2 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
@@ -3,8 +3,6 @@ function solution(n) {
     let sum;
     let count = 0; 
     
-    if(n < 1 || n > 10000)
-        throw new Error('Error');
     
     // 자연수 n을 표현하는 for문 사용하여 연산하기.
     for(let i = 1; i <= n / 2; i++){
