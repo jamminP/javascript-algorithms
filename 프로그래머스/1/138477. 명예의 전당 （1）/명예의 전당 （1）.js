a7b09cc074e5496b0f5c2ad10af956455bf5efc2
JAEMIN PARK <117453101+jamminP@users.noreply.github.com>
2025-05-22 15:47:39 +0900

    [level 1] Title: 명예의 전당 (1), Time: 2.44 ms, Memory: 34.2 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -1,24 +1,25 @@
 function solution(k, score) {
     var answer = [];
     
-    let count = 0;
-    let Honor = [];
-    for (data of score){
-        if(count < k){
-            Honor.push(data);
-            count++;
-            Honor.sort((a,b) => a - b);
-            answer.push(Honor[0]);
-            continue;
+    const data = [];
+    
+    score.reduce((acc,curr) => {
+        if(data.length < k){
+            data.push(curr);
+            data.sort((a,b) => a-b);
+            answer.push(data[0]);
+        }    
+        else{
+            if(data[0] < curr){
+                data.push(curr);
+                data.shift();
+                data.sort((a,b) => a-b);
+                answer.push(data[0]);
+            }else{
+                answer.push(data[0]);
+            }
         }
-        
-        if(Honor[0] < data){
-            Honor.shift();
-            Honor.push(data);
-            Honor.sort((a,b) => a - b);
-        }
-        answer.push(Honor[0]);
-    }
+    },[]);
     
     return answer;
 }
